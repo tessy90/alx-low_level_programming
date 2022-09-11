78e0c61c93af94e93b994698f047a79c462e88d6
tessy90 <loisidris@gmail.com>
2022-09-11 10:18:24 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,15 +7,8 @@
  */
 
 int main(void)
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Stat
+
