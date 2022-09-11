43643d161587f38a39dd5b5934f888cf47b23ab7
tessy90 <loisidris@gmail.com>
2022-09-11 10:30:19 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of C program
+ * Return:0
+ */
+
+ int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+ 
+  }
