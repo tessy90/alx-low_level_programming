8abe52a7d1f1086268482ef5fb61939a52de953f
tessy90 <loisidris@gmail.com>
2022-09-11 10:08:47 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point of C program
+ * Return:0
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Stat
