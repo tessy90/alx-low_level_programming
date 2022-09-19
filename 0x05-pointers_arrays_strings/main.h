506e6b7c95ac246b05448b38b6415205235c80cf
tessy90 <loisidris@gmail.com>
2022-09-19 04:12:40 -0700

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,10 @@
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#include "main.h"
+
+/**
+* reset_to_98 -> a function to update the reference of a pointer
+* @n: pointer to n to be updated
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
